92df8190e3f52dc39b85b28fb6c29c2299545418
Martin Xiao <martinxiao@augmentum.com.cn>
2015-09-08 17:30:09 +0800

    Compress css file by grunt-contrib-cssmin

--- src/frontend/Gruntfile.js
+++ src/frontend/Gruntfile.js
@@ -18,12 +18,40 @@ module.exports = function(grunt) {
         dest: './css/app.css',
       },
     },
+    // uglify: {
+    //   my_target: {
+    //     files: {
+    //       './js/a.min.js': ['./js/a.js']
+    //     }
+    //   }
+    // }
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'output.css': ['foo.css', 'bar.css']
+        }
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: './css',
+          src: ['*.css', '!*.min.css'],
+          dest: './css',
+          ext: '.min.css'
+        }]
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['sass', 'concat']);
+  grunt.registerTask('default', ['sass', 'concat', 'cssmin']);
 
 
 };
