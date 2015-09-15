c93425199068334d335b1a45a89cac7511bd3491
xiaomaolin <martin.xiao@outlook.com>
2015-09-15 23:42:03 +0800

    Try AMD optimize

--- src/frontend/Gulpfile.js
+++ src/frontend/Gulpfile.js
@@ -4,16 +4,24 @@ var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var rename = require("gulp-rename");
+var amdOptimize = require('amd-optimize');
 
 gulp.task('css', function() {
 	return gulp.src('./sass/**/*.scss')
-	  .pipe(sass().on('error', sass.logError))
-	  .pipe(autoprefixer(['last 2 versions']))
-    .pipe(concat('app.css'))
-	  .pipe(minifyCss())
-	  .pipe(rename({suffix: ".min"}))
-	  .pipe(gulp.dest('./css'));
+	    .pipe(sass().on('error', sass.logError))
+	    .pipe(autoprefixer(['last 2 versions']))
+   	    .pipe(concat('app.css'))
+	    .pipe(minifyCss())
+	    .pipe(rename({suffix: ".min"}))
+	    .pipe(gulp.dest('./css'));
 });
+
+// gulp.task('js', function () {
+//     return gulp.src('./js/*.js')
+//         .pipe(amdOptimize('main'))
+//         .pipe(concat('main-bundle.js'))
+//         .pipe(gulp.dest('./js/dist'));
+// });
 
 gulp.task('default', function () {
   gulp.watch('./sass/**/*.scss', ['css']);
